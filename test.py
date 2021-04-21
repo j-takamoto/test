7d63365490580d90d645e6103eea75a2ffef1abb
JIN TAKAMOTO <jtakamoto@mtc-sams.net>
2021-04-21 09:13:22 +0000

    test push

--- test.py
+++ test.py
@@ -1,3 +1,24 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/favicon.ico")
+def favicon():
+    return "favicon"
+
+@app.route("/")
+def index():
+    str = """
+        <html>
+            <head>
+                <title>GitPod Test</title>
+            </head>
+            <body>
+                <h1>Hello GitPod
+            </body>
+        </html>
+    """
+    return str
+
 if __name__ == "__main__":
-    for i in range(10):
-        print(str(i))
+    app.run(host="0.0.0.0", port=8000, debug=True)
